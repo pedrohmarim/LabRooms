c10cc6b60c3d124d06293b423cf463db314429a7
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2021-10-01 19:21:08 -0300

    #3 - teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react';
-import api from './services/backAPI';
+import React, { useEffect } from "react";
+import api from "./services/backAPI";
 
-export default function App(){
+export default function App() {
+  useEffect(() => {
+    api
+      .post("/test", {
+        headers: {
+          DTO: {
+            nome: "Pedro",
+            sobrenome: "Marim teste 123",
+          },
+        },
+      })
+      .then((res) => {
+        const { data } = res;
+        console.log(data);
+      });
+  }, []);
 
-  useEffect(()=>{
-    api.post('/test', {
-      headers:{
-        DTO:{
-          nome: 'Pedro',
-          sobrenome: 'Marim'
-        }
-      }
-    }).then(res=>{
-      const { data } = res;
-      console.log(data);
-    })
-  },[])
-
-  return (
-    <h1>App</h1>
-  )
+  return <h1>App</h1>;
 }
