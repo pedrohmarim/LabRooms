6f00eaaa5f718bb735e1946982114d67b4876dbd
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2021-10-01 19:25:27 -0300

    #teste

--- backend/controllers/test.js
+++ backend/controllers/test.js
@@ -1,20 +1,23 @@
-const testModel = require('../models/testModel');
+const testModel = require("../models/testModel");
 
 module.exports = {
-    async test(request, response){
-        const { nome, sobrenome } = request.body.headers.DTO;
+  async test(request, response) {
+    const { nome, sobrenome } = request.body.headers.DTO;
 
-        testModel.create({
-            nome,
-            sobrenome
-        }).then(()=>{
-            return response.json({message: 'Foi pro bd!!!!'})
-        }).catch(err =>{
-            return response.json({message: `deu erro ekew = ${err}`})
-        })
-        
-        // return(response.json({
-        //     nomeCompleto: nome + ' henrique'
-        // }));
-    }
-}
+    testModel
+      .create({
+        nome,
+        sobrenome,
+      })
+      .then(() => {
+        return response.json({ message: "Foi pro bdteste!!!!" });
+      })
+      .catch((err) => {
+        return response.json({ message: `deu erro ekew = ${err}` });
+      });
+
+    // return(response.json({
+    //     nomeCompleto: nome + ' henrique'
+    // }));
+  },
+};
