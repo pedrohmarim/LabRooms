05fed45d1cdc7c81d7441e0838a8f1ae9b0a7a67
SMARAPD - Pedro Cavelani <phmcavelani@smarapd.com.br>
2022-03-27 13:49:58 -0300

    #89 - remove .log

--- frontend/backend/controllers/UserController.js
+++ frontend/backend/controllers/UserController.js
@@ -170,7 +170,6 @@ module.exports = {
       const { username, email, cpf, phone, celphone, biography, socials } =
         request.body;
 
-      console.log(request.body);
       UserModel.findByIdAndUpdate(
         { _id },
         {
