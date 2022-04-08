92c6afa236141d8330d56800f90be140c3ca8ef3
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2022-04-08 16:22:30 -0300

    #113 - basic fixes

--- frontend/src/Pages/UserProfile/components/Tabs/sessions/PersonalInfo.component.js
+++ frontend/src/Pages/UserProfile/components/Tabs/sessions/PersonalInfo.component.js
@@ -31,7 +31,7 @@ const SocialRegister = ({
               <Tooltip
                 color={darkPallete.lightblue}
                 title='Editar Perfil'
-                defaultVisible={window.innerWidth < 1024}
+                defaultVisible
               >
                 <StyledButton
                   backgroundcolor='transparent'
