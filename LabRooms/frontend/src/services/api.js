c16cb8d27f972472e7523ed74eefc0d9cee72f88
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2022-05-16 16:37:20 -0300

    #163 - cors vercel6

--- LabRooms/frontend/src/services/api.js
+++ LabRooms/frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import Axios from "axios";
 
-const api = Axios.create({ baseURL: "http://localhost:4000" }); // ver se arruma isso aqui para .env
+const api = Axios.create({
+  baseURL: `${window.location.href.split(":3000")[0]}:4000`,
+}); // ver se arruma isso aqui para .env
 
 export default api;
--- LabRooms/server.js
+++ LabRooms/server.js
@@ -8,23 +8,8 @@ const path = require("path");
 
 connection();
 
-const corsOpts = {
-  origin: "*",
-
-  methods: ["GET", "POST"],
-
-  allowedHeaders: ["Content-Type"],
-};
-
-app.use(cors(corsOpts));
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(express.json());
+app.use(cors());
 app.listen(process.env.PORT);
 app.use(routesUrls);
