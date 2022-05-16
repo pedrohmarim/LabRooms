cbf0851a3be83f01065ab4c15dc414067888b42a
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2022-05-16 16:14:42 -0300

    #159 - cors vercel2

--- LabRooms/server.js
+++ LabRooms/server.js
@@ -3,13 +3,18 @@ const express = require("express");
 const app = express();
 const connection = require("./db");
 const routesUrls = require("./backRoutes/routes");
-const cors = require("cors");
 const path = require("path");
 
 connection();
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(express.json());
-app.use(cors());
 app.listen(process.env.PORT);
 app.use(routesUrls);
