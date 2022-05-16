2e5d24fb9e90b291f3eef381af665e09be414af2
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2022-05-16 16:20:24 -0300

    #162 - cors vercel5

--- LabRooms/db.js
+++ LabRooms/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = async function connection() {
   try {
     mongoose.connect(process.env.DB);
-    console.log('connected')
+    console.log("connected");
   } catch (error) {
     console.log(error, "could not connect to database");
   }
