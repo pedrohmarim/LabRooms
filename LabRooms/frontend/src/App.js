b092fc564f2eaa21659b8b23351cfa4d7697f60b
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2022-05-16 16:53:10 -0300

    trigger2

--- LabRooms/frontend/src/App.js
+++ LabRooms/frontend/src/App.js
@@ -3,8 +3,6 @@ import Routes from "./Routes";
 import "antd/dist/antd.css";
 
 const App = () => {
-  console.log("URL API1", `${window.location.href.split(":3000")[0]}:4000`);
-  console.log("URL API2", `${window.location.href}/imagem123`);
   return <Routes />;
 };
 
--- LabRooms/frontend/src/services/api.js
+++ LabRooms/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const api = Axios.create({
-  baseURL: `${window.location.href}:4000`,
+  baseURL: `${window.location.href.slice(0, -1)}:4000`,
 });
 
 export default api;
