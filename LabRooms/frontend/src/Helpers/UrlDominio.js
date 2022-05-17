47645bbdcb6c51b9e15cf64b3c308f61aabfa22e
Pedro Henrique Marim Cavelani <62761711+trilp123@users.noreply.github.com>
2022-05-17 15:32:24 -0300

    #174 - railway url

--- LabRooms/frontend/src/Helpers/UrlDominio.js
+++ LabRooms/frontend/src/Helpers/UrlDominio.js
@@ -1,21 +1,15 @@
 export function MontaUrlDominio() {
   const isDevelopment = window.location.host.indexOf("3000");
 
-  console.log(isDevelopment);
-
   if (isDevelopment !== -1) {
     const urlDevelopment = `http://${
       window.location.host.split(":3000")[0]
     }:4000/`;
 
-    console.log("returning url API development", urlDevelopment);
-
     return urlDevelopment;
   }
 
-  const urlProd = `https://${window.location.host}:4000`;
-
-  console.log("returning url development", urlProd);
+  const urlProd = `https://labrooms.up.railway.app`;
 
   return urlProd;
 }
